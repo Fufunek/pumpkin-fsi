f9051be111de1304b720d82e7f3fc635b349f7aa
ArcasCZ <VydraMan@seznam.cz>
2022-02-16 21:24:01 +0100

    Soccer: Delete old repost info

--- soccer/module.py
+++ soccer/module.py
@@ -206,6 +206,7 @@ class Soccer(commands.Cog):
             history_word = self._get_word(history_message)
 
             if history_word == word:
+                await self._delete_repost(message)
                 await self._report_repost(message, history_message, word)
                 return
 
