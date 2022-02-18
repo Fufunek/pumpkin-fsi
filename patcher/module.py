764509936b224291807b6ee798e6963a1c291501
ArcasCZ <VydraMan@seznam.cz>
2022-02-18 13:04:21 +0100

    Patcher: Patch date formating

--- patcher/module.py
+++ patcher/module.py
@@ -1,6 +1,10 @@
+import datetime
+import hashlib
+import inspect
+
 from nextcord.ext import commands, tasks
 
-from pie import logger, check
+from pie import logger, utils
 
 guild_log = logger.Guild.logger()
 bot_log = logger.Bot.logger()
@@ -11,21 +15,42 @@ ADD_ACL = ["gn", "selfunverify"]
 class Patcher(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.date_check = "16572801b42921d1f9f9b1279dc75dba"
+        self.datetime_check = "23af3078e84e9f0dfbef619dfde789b4"
 
         self.patch.start()
 
     @tasks.loop(seconds=10.0, count=1)
     async def patch(self):
-        out = "Patched commands: "
-        for command in ADD_ACL:
-            if await self._add_acl(command):
-                out += f"`{command}` "
+        date_source = inspect.getsource(utils.time.format_date)
+        date_hash = hashlib.md5(date_source.encode()).hexdigest()
 
-        await bot_log.info(
-            None,
-            None,
-            out,
-        )
+        log = f"Original date function hash is {date_hash}."
+
+        if date_hash == self.date_check:
+            print(log + " Hash matches. Patching.")
+            utils.time.format_date = Patcher.fix_format_date
+        else:
+            print(log + " Hash does not match!")
+            await bot_log.error(
+                None, None, "Hash for function 'utils.time.format_date' does not match!"
+            )
+
+        datetime_source = inspect.getsource(utils.time.format_datetime)
+        datetime_hash = hashlib.md5(datetime_source.encode()).hexdigest()
+
+        log = f"Original datetime function hash is {datetime_hash}."
+
+        if datetime_hash == self.datetime_check:
+            print(log + " Hash matches. Patching.")
+            utils.time.format_datetime = Patcher.fix_format_datetime
+        else:
+            print(log + " Hash does not match!")
+            await bot_log.error(
+                None,
+                None,
+                "Hash for function 'utils.time.format_datetime' does not match!",
+            )
 
     @patch.before_loop
     async def before_patch(self):
@@ -35,18 +60,15 @@ class Patcher(commands.Cog):
         print("Waiting for bot being ready to fix some deFEKTs")
         await self.bot.wait_until_ready()
 
-    async def _add_acl(self, command_name):
-        command = self.bot.get_command(command_name)
-        if command is None:
-            await bot_log.error(
-                None,
-                None,
-                f"Can't add ACL to command {command_name}. Command not found.",
-            )
-            return
+    @staticmethod
+    def fix_format_date(timestamp: datetime.datetime) -> str:
+        """Convert timestamp to date."""
+        return timestamp.strftime("%d-%m-%Y ")
 
-        command.add_check(check.acl)
-        return True
+    @staticmethod
+    def fix_format_datetime(timestamp: datetime.datetime) -> str:
+        """Convert timestamp to date and time."""
+        return timestamp.strftime("%d-%m-%Y %H:%M:%S")
 
 
 def setup(bot) -> None:
